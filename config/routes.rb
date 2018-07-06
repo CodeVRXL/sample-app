6a014ac10290685da9d97b71a87249561b2cdab3
CodeVRXL <huy.the.hoang@gmail.com>
2018-07-06 16:03:41 -0700

    added about action and tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'static_pages/home' # maps requests for the URL /static_pages/home to the home action in the Static Pages controller.
   get 'static_pages/help'
+  get 'static_pages/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
 end
